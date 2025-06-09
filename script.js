1c57eb782896b442f2af4b5ee834886ff8672700
Ekio <141391736+Eki213@users.noreply.github.com>
2025-06-09 17:05:21 -0300

    Implement logic to add books to the library

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(crypto.randomUUID(), title, author, pages, read);
+    myLibrary.push(book);
+}
